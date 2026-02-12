e96abe019acfae5aa7500f4bfd0f45eb451e2788
Mohammed Ateeq Ur Rahman <mdateequrrahma123@gmail.com>
2026-02-12 14:31:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6812991960-ca2c0",
+  "appId": "1:489367848982:web:e8f8fb6d060f4740aecd37",
+  "apiKey": "AIzaSyAl1AkXYrtPt5TNi8QGMySFZr2bqefQDY8",
+  "authDomain": "studio-6812991960-ca2c0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "489367848982"
+};
